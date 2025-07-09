7000dad5fd83bc5ca56a17f5e5d59169ff9c8e90
mvghasty <fasmagoric.dev@gmail.com>
2025-07-09 17:19:11 -0300

    0.2 PATCH: New commands and a manifest.json for the project

--- main.c
+++ main.c
@@ -10,6 +10,30 @@ int num_lines = 0;
 int current_line = 0;
 FILE *output_file = NULL;
 char filename[256] = "foo._";
+char version[32] = "unknown";
+
+void load_version(const char *json_file) {
+    FILE *f = fopen(json_file, "r");
+    if (!f) return;
+
+    char line[256];
+    while (fgets(line, sizeof(line), f)) {
+        if (strstr(line, "\"version\"")) {
+            char *start = strchr(line, ':');
+            if (start) {
+                start += 1;
+                while (*start == ' ' || *start == '\"') start++;
+                char *end = strchr(start, '\"');
+                if (end) *end = '\0';
+                strncpy(version, start, sizeof(version) - 1);
+                version[sizeof(version) - 1] = '\0';
+                break;
+            }
+        }
+    }
+
+    fclose(f);
+}
 
 void append() {
     char line[MAX_LEN];
@@ -44,18 +68,24 @@ void numbered_print_current() {
         printf("?\n");
         return;
     }
-    printf("%d\t%s", current_line + 1, buffer[current_line]);
+    printf("\e[0;33m%d\e[0m\t%s", current_line + 1, buffer[current_line]);
 }
 
 void print_all() {
     for (int i = 0; i < num_lines; i++) {
-        printf("%s", buffer[i]);
+        if (i == current_line)
+            printf("* %s", buffer[i]);
+        else
+            printf("  %s", buffer[i]);
     }
 }
 
 void numbered_print_all() {
     for (int i = 0; i < num_lines; i++) {
-        printf("%d\t%s", i + 1, buffer[i]);
+        if (i == current_line)
+            printf("\e[1;36m*\e[0m \e[0;33m%d\e[0m\t%s", i + 1, buffer[i]);
+        else
+            printf("  \e[0;33m%d\e[0m\t%s", i + 1, buffer[i]);
     }
 }
 
@@ -75,6 +105,9 @@ void delete_current() {
 }
 
 int main(int argc, char *argv[]) {
+    system("clear");
+    load_version("manifest.json");
+
     if (argc > 1) {
         strncpy(filename, argv[1], sizeof(filename) - 1);
         filename[sizeof(filename) - 1] = '\0';
@@ -101,15 +134,36 @@ int main(int argc, char *argv[]) {
         current_line = num_lines - 1;
     }
 
-    printf("cog 0.1 - simple line editor\n");
+    printf("cog %s - simple line editor\n", version);
     printf("Licensed under the MIT License\n");
-    printf("Opened file: %s\n", filename);
+    printf("Opened file: %s\n\n", filename);
 
-    char cmd[10];
+    char cmd[512]; 
 
     while (1) {
         printf(": ");
         if (fgets(cmd, sizeof(cmd), stdin) == NULL) break;
+
+        size_t len = strlen(cmd);
+        if (len > 0 && cmd[len - 1] == '\n') {
+            cmd[len - 1] = '\0';
+        }
+
+        if (strncmp(cmd, "bsh$", 4) == 0) {
+            char *bash_cmd = cmd + 4;
+            if (strlen(bash_cmd) > 0) {
+                int result = system(bash_cmd);
+                printf("[exit code: %d]\n", result);
+            } else {
+                printf("No shell command provided.\n");
+            }
+            continue;
+        }
+
+        if (strcmp(cmd, "clear") == 0) {
+            system("clear");
+            continue;
+        }
 
         switch (cmd[0]) {
             case 'a':
@@ -133,8 +187,16 @@ int main(int argc, char *argv[]) {
                 break;
             case 'q':
                 goto end;
-            case '\n':
+            case '\0':
                 if (current_line < num_lines - 1) current_line++;
+                break;
+            case '-':
+                if (current_line < num_lines - 1) current_line++;
+                else printf("At end of buffer.\n");
+                break;
+            case '+':
+                if (current_line > 0) current_line--;
+                else printf("At start of buffer.\n");
                 break;
             default:
                 printf("Unknown command.\n");
